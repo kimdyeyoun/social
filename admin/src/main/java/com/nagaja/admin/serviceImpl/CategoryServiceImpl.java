c838a5602855b13ff2d9407d5a29d10ed6e1b587
eodyd <eodyd.pref@gmail.com>
2022-07-15 19:12:51 +0900

    카테고리 관리 조회/검색/상세조회/페이징/수정 하위카테고리 관리 조회/검색/상세조회

--- admin/src/main/java/com/nagaja/admin/serviceImpl/CategoryServiceImpl.java
+++ admin/src/main/java/com/nagaja/admin/serviceImpl/CategoryServiceImpl.java
@@ -1,13 +1,14 @@
 package com.nagaja.admin.serviceImpl;
 
 import com.nagaja.admin.dto.BoardCategoryDto;
-import com.nagaja.admin.dto.CompanyDto;
-import com.nagaja.admin.dto.CompanyInfoDto;
+import com.nagaja.admin.dto.CategoryUpdateDto;
 import com.nagaja.admin.entity.BoardCategory;
 import com.nagaja.admin.entity.Pagination;
 import com.nagaja.admin.mapper.CategoryMapper;
 import com.nagaja.admin.service.CategoryService;
+import com.nagaja.admin.util.AWSUploader;
 import com.nagaja.admin.util.MyUtils;
+import com.nagaja.admin.util.Status;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,8 @@ import java.util.List;
 public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryMapper categoryMapper;
+
+    private final AWSUploader AWSUploader;
 
     //TODO 카테고리 리스트 설렉트
     @Override
@@ -30,5 +33,61 @@ public class CategoryServiceImpl implements CategoryService {
         List<BoardCategory> boardCategoryList = categoryMapper.categoryList(pagination.getOffset(), pagination.getLimit());
 
         return BoardCategoryDto.builder().boardCategoryList(boardCategoryList).pagination(pagination).build();
+    }
+
+    //TODO 하위 카테고리 리스트 설렉트
+    @Override
+    public BoardCategoryDto selectLowerRankCategory(BoardCategoryDto dto)
+    {
+        int count = categoryMapper.lowerRankCategoryCount(dto.getBoardCategoryId());
+
+        Pagination pagination = MyUtils.Paging(count, dto.getPageNum(), dto.getLimit());
+
+        List<BoardCategory> boardCategoryList = categoryMapper.lowerRankCategoryList(dto.getBoardCategoryId(), pagination.getOffset(), pagination.getLimit());
+
+        return BoardCategoryDto.builder().boardCategoryList(boardCategoryList).pagination(pagination).build();
+    }
+
+    //TODO 카테고리 디테일
+    @Override
+    public BoardCategory detailCategory(int boardCategoryId)
+    {
+        return categoryMapper.detailCategory(boardCategoryId);
+    }
+
+    //TODO 카테고리 업데이트
+    @Override
+    public int updateCategory(CategoryUpdateDto dto)
+    {
+        System.out.println(dto.getFile());
+        if (dto.getFile() != null && !dto.getFile().isEmpty())
+        {
+
+            String aws_name = AWSUploader.changeFileName(dto.getFile());
+            boolean uploadAWS = AWSUploader.uploadToWithNameAwsS3("nagaja/category", dto.getFile(), aws_name);
+
+            if(uploadAWS)
+            {
+                if(dto.getBoardCategoryImageName() != null)
+                {
+                    String[] imageName = dto.getBoardCategoryImageName().split("/");
+                    String path = imageName[0] + "/" + imageName[1];
+                    boolean deleteAWS = AWSUploader.deleteFileInAwsByKey(path, imageName[2]);
+                }
+
+                String file_name = "nagaja/category/" + aws_name;
+
+                dto.setBoardCategoryImageName(file_name);
+                dto.setBoardCategoryImageOrigin(dto.getFile().getOriginalFilename());
+            }
+
+        }
+        int result = categoryMapper.updateCategory(dto);
+
+        if (result == 0)
+        {
+            return Status.ZERO;
+        }
+        return Status.FIRST;
     }
 }
